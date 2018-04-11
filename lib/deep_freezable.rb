7059a033571bba9251b97664352c20ba4f3eff18
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2018-04-11 13:20:48 +0900

    deep_freeze追加

---
+++ lib/deep_freezable.rb
@@ -0,0 +1,17 @@
+module DeepFreezable
+  def deep_freeze(array_or_hash)
+    case array_or_hash
+    when Array
+      array_or_hash.each do |element|
+        element.freeze
+      end
+    when Hash
+      array_or_hash.each do |key, value|
+        key.freeze
+        value.freeze
+      end
+    end
+    # 配列自身をfreeze(かつ、これがメソッドの戻り値となる)
+    array_or_hash.freeze
+  end
+end
